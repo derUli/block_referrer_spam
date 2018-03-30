cfc211ceb334dc9b03b5380098a1ed3b792e0670
Ulrich Schmidt <support@ulicms.de>
2018-03-30 15:22:36 +0200

    Zu allgemeine Einträge aus der Liste entfernt, Wenn geblockt wird, wird nun die Regel angezeigt.

--- src/content/modules/block_referrer_spam/BlockReferrerSpam.php
+++ src/content/modules/block_referrer_spam/BlockReferrerSpam.php
@@ -1,36 +1,52 @@
 <?php
-class BlockReferrerSpam extends Controller {
-	private $moduleName;
-	public function __construct() {
-		$this->moduleName = basename ( dirname ( __FILE__ ) );
-	}
-	// Check for referrer spam
-	public function beforeInit() {
-		if ($this->isReferrerSpam ( get_referrer () )) {
-			HTMLResult ( "Request blocked due referrer spam.", 403 );
-		}
-	}
-	// Get list of forbidden words as array
-	public function getList() {
-		$file = ModuleHelper::buildModuleRessourcePath ( $this->moduleName, "list.txt", true );
-		$list = file_get_contents ( $file );
-		$list = normalizeLN ( $list, "\n" );
-		$list = explode ( "\n", $list );
-		$list = array_map ( "trim", $list );
-		$list = array_filter ( $list, "strlen" );
-		$list = array_filter ( $list, "strtolower" );
-		sort ( $list );
-		return $list;
-	}
-	// returns true if a referrer is a spam referrer
-	public function isReferrerSpam($referrer) {
-		$isSpam = false;
-		$list = $this->getList ();
-		foreach ( $list as $spam ) {
-			if (str_contains ( $spam, strtolower ( $referrer ) )) {
-				$isSpam = true;
-			}
-		}
-		return $isSpam;
-	}
+use phpDocumentor\Reflection\Types\Nullable;
+
+class BlockReferrerSpam extends Controller
+{
+
+    private $moduleName;
+
+    public function __construct()
+    {
+        $this->moduleName = basename(dirname(__FILE__));
+    }
+
+    // Check for referrer spam
+    public function beforeInit()
+    {
+        $isSpam = $this->isReferrerSpam(get_referrer());
+        if ($isSpam) {
+            $text = "Request blocked due referrer spam.\n";
+            $text .= "Rule: {$isSpam}";
+            $text = nl2br($text);
+            HTMLResult($text, 403);
+        }
+    }
+
+    // Get list of forbidden words as array
+    public function getList()
+    {
+        $file = ModuleHelper::buildModuleRessourcePath($this->moduleName, "list.txt", true);
+        $list = file_get_contents($file);
+        $list = normalizeLN($list, "\n");
+        $list = explode("\n", $list);
+        $list = array_map("trim", $list);
+        $list = array_filter($list, "strlen");
+        $list = array_filter($list, "strtolower");
+        sort($list);
+        return $list;
+    }
+
+    // returns true if a referrer is a spam referrer
+    public function isReferrerSpam($referrer)
+    {
+        $isSpam = null;
+        $list = $this->getList();
+        foreach ($list as $spam) {
+            if (str_contains($spam, strtolower($referrer))) {
+                $isSpam = $spam;
+            }
+        }
+        return $isSpam;
+    }
 }
