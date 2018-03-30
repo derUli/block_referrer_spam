8954fb448540417a691d26741f95d5c129470874
Ulrich Schmidt <support@ulicms.de>
2018-03-30 16:47:58 +0200

    Tests an neue Liste angepasst

--- src/content/modules/block_referrer_spam/tests/BlockReferrerSpamTest.php
+++ src/content/modules/block_referrer_spam/tests/BlockReferrerSpamTest.php
@@ -1,17 +1,23 @@
 <?php
-class BlockReferrerTest extends PHPUnit_Framework_TestCase {
-	public function testSpam() {
-		$controller = ModuleHelper::getMainController ( "block_referrer_spam" );
-		$this->assertTrue ( $controller->isReferrerSpam ( "http://www.awesome-casino.ru" ) );
-		$this->assertTrue ( $controller->isReferrerSpam ( "https://cheap-cialis.com/buy" ) );
-		$this->assertTrue ( $controller->isReferrerSpam ( "/best-insurance" ) );
-		$this->assertTrue ( $controller->isReferrerSpam ( "best/bet" ) );
-	}
-	public function testNoSpam() {
-		$controller = ModuleHelper::getMainController ( "block_referrer_spam" );
-		$this->assertFalse ( $controller->isReferrerSpam ( "http://www.google.de" ) );
-		$this->assertFalse ( $controller->isReferrerSpam ( "https://en.ulicms.de/current_versions.html" ) );
-		$this->assertFalse ( $controller->isReferrerSpam ( "" ) );
-		$this->assertFalse ( $controller->isReferrerSpam ( null ) );
-	}
+
+class BlockReferrerTest extends PHPUnit_Framework_TestCase
+{
+    
+    public function testSpam()
+    {
+        $controller = ModuleHelper::getMainController("block_referrer_spam");
+        $this->assertEquals(".win", $controller->isReferrerSpam("http://iphone.win"));
+        $this->assertEquals("buttons-for-website.com", $controller->isReferrerSpam("https://buttons-for-website.com/buy-cheap-viagra"));
+        $this->assertEquals("youporn-ru.com", $controller->isReferrerSpam("https://youporn-ru.com/great-tits"));
+        $this->assertEquals(".top", $controller->isReferrerSpam("download.top"));
+    }
+    
+    public function testNoSpam()
+    {
+        $controller = ModuleHelper::getMainController("block_referrer_spam");
+        $this->assertNull($controller->isReferrerSpam("http://www.google.de"));
+        $this->assertNull($controller->isReferrerSpam("https://en.ulicms.de/current_versions.html"));
+        $this->assertNull($controller->isReferrerSpam(""));
+        $this->assertNull($controller->isReferrerSpam(null));
+    }
 }
