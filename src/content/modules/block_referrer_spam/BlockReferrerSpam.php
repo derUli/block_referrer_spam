2ab3634ba363a05c4fe59237ed1dfaa290a216ff
Der Uli im Netz <admin@deruli.de>
2020-06-25 20:41:01 +0200

    update

--- src/content/modules/block_referrer_spam/BlockReferrerSpam.php
+++ src/content/modules/block_referrer_spam/BlockReferrerSpam.php
@@ -1,52 +1,57 @@
 <?php
+
 use phpDocumentor\Reflection\Types\Nullable;
 
-class BlockReferrerSpam extends Controller
-{
+class BlockReferrerSpam extends Controller {
 
     private $moduleName;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->moduleName = basename(dirname(__FILE__));
     }
 
     // Check for referrer spam
-    public function beforeInit()
-    {
+    public function beforeInit() {
+        $message = $this->_getMessage();
+        if ($message) {
+            HTMLResult($message, 403);
+        }
+    }
+
+    public function _getMessage(): ?string {
+        $text = null;
         $isSpam = $this->isReferrerSpam(get_referrer());
         if ($isSpam) {
             $text = "Request blocked due referrer spam.\n";
             $text .= "Rule: {$isSpam}";
             $text = nl2br($text);
-            HTMLResult($text, 403);
         }
+        return $text;
     }
 
     // Get list of forbidden words as array
-    public function getList()
-    {
-        $file = ModuleHelper::buildModuleRessourcePath($this->moduleName, "list.txt", true);
-        $list = file_get_contents($file);
-        $list = normalizeLN($list, "\n");
-        $list = explode("\n", $list);
-        $list = array_map("trim", $list);
-        $list = array_filter($list, "strlen");
-        $list = array_filter($list, "strtolower");
+    public function getList() {
+        $file = ModuleHelper::buildModuleRessourcePath(
+                        $this->moduleName,
+                        "vendor/matomo/referrer-spam-blacklist/spammers.txt",
+                        true
+        );
+        $list = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
         sort($list);
         return $list;
     }
 
     // returns true if a referrer is a spam referrer
-    public function isReferrerSpam($referrer)
-    {
+    public function isReferrerSpam($referrer) {
         $isSpam = null;
         $list = $this->getList();
         foreach ($list as $spam) {
-            if (str_contains($spam, strtolower($referrer))) {
+            if (str_contains(strtolower($referrer), $spam)) {
                 $isSpam = $spam;
             }
         }
         return $isSpam;
     }
+
 }
--- src/content/modules/block_referrer_spam/tests/BlockReferrerSpamTest.php
+++ src/content/modules/block_referrer_spam/tests/BlockReferrerSpamTest.php
@@ -1,23 +1,43 @@
 <?php
 
-class BlockReferrerTest extends \PHPUnit\Framework\TestCase
-{
-    
-    public function testSpam()
-    {
-        $controller = ModuleHelper::getMainController("block_referrer_spam");
-        $this->assertEquals(".win", $controller->isReferrerSpam("http://iphone.win"));
+use Spatie\Snapshots\MatchesSnapshots;
+
+class BlockReferrerSpamTest extends \PHPUnit\Framework\TestCase {
+
+    use MatchesSnapshots;
+
+    protected function tearDown(): void {
+        if (isset($_SERVER["HTTP_REFERER"])) {
+            unset($_SERVER["HTTP_REFERER"]);
+        }
+    }
+
+    public function testIsReferrerSpamReturnsTrue(): void {
+        $controller = new BlockReferrerSpam();
+        $this->assertEquals("casino-v.site", $controller->isReferrerSpam("http://casino-v.site"));
         $this->assertEquals("buttons-for-website.com", $controller->isReferrerSpam("https://buttons-for-website.com/buy-cheap-viagra"));
         $this->assertEquals("youporn-ru.com", $controller->isReferrerSpam("https://youporn-ru.com/great-tits"));
-        $this->assertEquals(".top", $controller->isReferrerSpam("download.top"));
+        $this->assertEquals("constanceonline.top", $controller->isReferrerSpam("https://constanceonline.top/foo"));
     }
-    
-    public function testNoSpam()
-    {
+
+    public function testIsReferrerSpamReturnsFalse(): void {
         $controller = ModuleHelper::getMainController("block_referrer_spam");
         $this->assertNull($controller->isReferrerSpam("http://www.google.de"));
         $this->assertNull($controller->isReferrerSpam("https://en.ulicms.de/current_versions.html"));
         $this->assertNull($controller->isReferrerSpam(""));
         $this->assertNull($controller->isReferrerSpam(null));
     }
+
+    public function testGetMessageReturnsString() {
+        $_SERVER["HTTP_REFERER"] = "https://youporn-ru.com/great-tits";
+        $controller = new BlockReferrerSpam();
+        $this->assertMatchesTextSnapshot($controller->_getMessage());
+    }
+
+    public function testGetMessageReturnsNull() {
+        $_SERVER["HTTP_REFERER"] = "https://www.google.de";
+        $controller = new BlockReferrerSpam();
+        $this->assertNull($controller->_getMessage());
+    }
+
 }
